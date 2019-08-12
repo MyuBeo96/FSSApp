2688e358f46ac02ad85cc909365a8b586013f38e
Vũ Diệu Linh <linh.vu@fssc.com.vn>
2019-08-12 18:30:44 +0700

    man hinh create

---
+++ app/src/main/java/com/myubeo/fssapp/design/TextDay.java
@@ -0,0 +1,130 @@
+package com.myubeo.fssapp.design;
+
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.app.Service;
+import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import com.myubeo.fssapp.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class TextDay extends LinearLayout {
+
+    Context context;
+    EditText edt_EdittextDate;
+    ImageButton btn_EdittextDate;
+    final Calendar myCalendar;
+    private String current = "";
+    private String ddmmyyyy = "DDMMYYYY";
+    private Calendar cal = Calendar.getInstance();
+
+    public TextDay(final Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Service.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.textview_day, this);
+        myCalendar = Calendar.getInstance();
+        edt_EdittextDate = findViewById(R.id.edt_EdittextDate);
+        btn_EdittextDate = findViewById(R.id.btn_EdittextDate);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        edt_EdittextDate.setText(sdf.format(new Date()));
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+        };
+
+        btn_EdittextDate.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(context, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+
+        edt_EdittextDate.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().equals(current)) {
+                    String clean = s.toString().replaceAll("[^\\d.]|\\.", "");
+                    String cleanC = current.replaceAll("[^\\d.]|\\.", "");
+
+                    int cl = clean.length();
+                    int sel = cl;
+                    for (int i = 2; i <= cl && i < 6; i += 2) {
+                        sel++;
+                    }
+                    if (clean.equals(cleanC)) sel--;
+
+                    if (clean.length() < 8) {
+                        clean = clean + ddmmyyyy.substring(clean.length());
+                    } else {
+                        int day = Integer.parseInt(clean.substring(0, 2));
+                        int mon = Integer.parseInt(clean.substring(2, 4));
+                        int year = Integer.parseInt(clean.substring(4, 8));
+
+                        mon = mon < 1 ? 1 : mon > 12 ? 12 : mon;
+                        cal.set(Calendar.MONTH, mon - 1);
+                        year = (year < 1900) ? 1900 : (year > 2100) ? 2100 : year;
+                        cal.set(Calendar.YEAR, year);
+
+                        day = (day > cal.getActualMaximum(Calendar.DATE)) ? cal.getActualMaximum(Calendar.DATE) : day;
+                        clean = String.format("%02d%02d%02d", day, mon, year);
+                    }
+
+                    clean = String.format("%s/%s/%s", clean.substring(0, 2),
+                            clean.substring(2, 4),
+                            clean.substring(4, 8));
+
+                    sel = sel < 0 ? 0 : sel;
+                    current = clean;
+                    edt_EdittextDate.setText(current);
+                    edt_EdittextDate.setSelection(sel < current.length() ? sel : current.length());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+    }
+
+    private void updateLabel() {
+        String myFormat = "dd/MM/yyyy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        edt_EdittextDate.setText(sdf.format(myCalendar.getTime()));
+    }
+}
--- app/src/main/java/com/myubeo/fssapp/fragment/CreateFragment.java
+++ app/src/main/java/com/myubeo/fssapp/fragment/CreateFragment.java
@@ -4,17 +4,128 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.myubeo.fssapp.R;
 
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class CreateFragment extends Fragment {
+
+    SeekBar seekbar_morning;
+    TextView time_morning;
+    EditText duration_Hour;
+    EditText duration_Minutes;
+    int hourStarts = 8;
+    int minuteStarts = 30;
+    int hourMorning = 12;
+    int minuteMorning = 0;
+    int duration = 3;
+
+    int timeWork;
+    int hours;
+    int minutes;
+
+    private final String pattern = "HH:mm";
+    private final String startTime = "08:30";
+    private final String finishTime = "12:00";
+    private DateFormat df = new SimpleDateFormat(pattern);
+    private DateFormat showHourDf = new SimpleDateFormat("HH");
+    private DateFormat showMinuteDf = new SimpleDateFormat("mm");
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_create, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        seekbar_morning = view.findViewById(R.id.seekbar_morning);
+        time_morning = view.findViewById(R.id.time_morning);
+        duration_Hour = view.findViewById(R.id.duration_Hour);
+        duration_Minutes = view.findViewById(R.id.duration_Minutes);
+
+        seekbar_morning.setProgress(210);
+        seekbar_morning.incrementProgressBy(15);
+
+//        time_morning.setText(String.valueOf(hourStarts)+ "h" + String.valueOf(minuteStarts) + " - " + String.valueOf(hourMorning) +
+//                "h" + ltrim(String.valueOf(minuteMorning), "0", 2));
+
+        String value = startTime + " - " + finishTime;
+        time_morning.setText(value);
+//
+//        duration_Minutes.setText(String.valueOf(minuteStarts));
+//        duration_Hour.setText(String.valueOf(duration));
+        initListener();
+
+    }
+
+    private void initListener(){
+        seekbar_morning.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progress = progress / 15;
+                progress = progress * 15;
+
+                long progressInMilliSec = progress * 60 * 1000; //progress = 15 minutes
+
+                try {
+                    long timeOnPoint = df.parse(startTime).getTime() + progressInMilliSec;
+                    Date dateOnPoint = new Date(timeOnPoint);
+                    Date midnight = df.parse("00:00");
+
+                    time_morning.setText(startTime + " - " + df.format(dateOnPoint));
+                    duration_Hour.setText(showHourDf.format(new Date (midnight.getTime() + progressInMilliSec)));
+                    duration_Minutes.setText(showMinuteDf.format(new Date (midnight.getTime() + progressInMilliSec)));
+                    DateFormat dffff = new SimpleDateFormat("HH:mm:ss");
+//                    Log.d("test", "onProgressChangedsdsdsd: " + dffff.format(caculatedTime));
+
+                } catch (ParseException e) {
+//                    e.printStackTrace();
+                    //ko the loi
+                }
+//                    timeWork = 510 + progress;
+//                    hours = timeWork / 60;
+//                    minutes = (timeWork - hours * 60) % 60;
+//                    time_morning.setText(String.valueOf(hourStarts)+ "h" + String.valueOf(minuteStarts) + " - " + String.valueOf(hours) +
+//                        "h" + ltrim(String.valueOf(minutes), "0", 2));
+//                    duration_Minutes.setText(String.valueOf(progress % 60));
+//                    duration_Hour.setText(String.valueOf(progress / 60));
+
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    private String ltrim(String input, String filledCharacter, int targetLength) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("");
+        for (int i = 1 ; i <= targetLength - input.length() ; i++) {
+            sb.append(filledCharacter);
+        }
+        sb.append(input);
+        return sb.toString();
+    }
+
 }
